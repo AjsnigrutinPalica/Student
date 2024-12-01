5ac6c318f9a74ef9bfdd2ac8e545395e98511680
ivan.ragac032@gmail.com <ivan.ragac032@gmail.com>
2024-12-01 21:27:25 +0100

    kraj

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
-﻿namespace Student
+﻿public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Student ivan = new Student();
+        ivan.IspisPodataka();
     }
+
 }
---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿ public class Student
+{
+    public string ime;
+    public string prezime;
+
+    public Student(string ime = "Ivan", string prezime= "Ragac")
+    {
+        this.ime = ime;
+        this.prezime = prezime;
+        Console.WriteLine($"{ime}{prezime} je stvoren/a");
+    }
+    public void IspisPodataka()
+    {
+        Console.WriteLine(ime, prezime);
+    }
+
+
+}
